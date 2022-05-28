741e84fa2c231e0d1f82118ffc60d7800647a647
GorbachyovKS <kirill_gorbachyov050201@mail.ru>
2022-05-28 15:20:35 +0300

    buildHeader

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,26 @@
 <template>
-  <h1>Hello world</h1>
+  <div class="wrapper">
+    <my-header></my-header>
+  </div>
 </template>
 
 <script>
-export default {};
+import MyHeader from "./components/MyHeader.vue";
+export default {
+  components: {
+    MyHeader,
+  },
+};
 </script>
 
 <style>
+body {
+  background-color: #262336;
+  font-family: Verdana, sans-serif;
+  color: #fff;
+}
+.wrapper {
+  display: flex;
+  justify-content: center;
+}
 </style>
---
+++ src/components/MyHeader.vue
@@ -0,0 +1,15 @@
+<template>
+  <header class="header">
+    <h1>TO DO LIST</h1>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.header {
+  font-size: 1.1rem;
+}
+</style>
